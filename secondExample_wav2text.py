6e1c500f536d9c15897a194d4de516c3e3c6c81b
Vadim Pechenin <vadim.pechenin2015@gmail.com>
2023-12-12 16:57:57 +0400

    Add Katya disser speech

--- secondExample_wav2text.py
+++ secondExample_wav2text.py
@@ -7,15 +7,15 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 #Необходимо сначала сконвертировать waw файл
 
-def wav2text(sound, r, mydoc,folder_name, language="ru-RU"):
+def wav2text(sound, r, mydoc,folder_name, file_name, language="ru-RU"):
     # split audio sound where silence is 700 miliseconds or more and get chunks
     chunks = split_on_silence(sound,
                               # experiment with this value for your target audio file
-                              min_silence_len=500,
+                              min_silence_len=1000,
                               # adjust this per requirement
-                              silence_thresh=sound.dBFS - 15,
+                              silence_thresh= -25,#sound.dBFS -15
                               # keep the silence for 1 second, adjustable as well
-                              keep_silence=500,
+                              keep_silence=1000,
                               )
     # process each chunk
     for i, audio_chunk in enumerate(chunks, start=1):
@@ -35,17 +35,21 @@ def wav2text(sound, r, mydoc,folder_name, language="ru-RU"):
                 print("Error:", str(e))
             else:
                 text = f"{text.capitalize()}. "
-                print(" -- :", text)
+                #print(" -- :", text)
+                print(text)
                 mydoc.add_paragraph(text)
-        mydoc.save("doc\\speech_BOL.docx")
-        print("Время выполнения: ")
-        print(datetime.now() - start_time)
+        mydoc.save("doc\\"+ file_name[:-4] + ".docx")
+        #print("Время выполнения: ")
+        #print(datetime.now() - start_time)
 
 folder_name = "D:\\PYTHON\\Programms\\audio"
 if (1==0):
 	file_name = "speech.waw"
 else:
-	file_name = "speech_BOL.wav"
+	#file_name = "speech_BOL.wav"
+    #file_name = "speech_EK_NTS.wav"
+    #file_name = "WS310980.wav"
+    file_name = "WS310980.wav"
 AUDIO_FILE  = os.path.join(folder_name,file_name)
 # open the audio file using pydub
 sound = AudioSegment.from_wav(AUDIO_FILE)
@@ -56,4 +60,4 @@ sound = AudioSegment.from_wav(AUDIO_FILE)
 mydoc = docx.Document()
 # create a speech recognition object
 r = sr.Recognizer()
-wav2text(sound, r, mydoc, folder_name, "ru-RU")
+wav2text(sound, r, mydoc, folder_name, file_name, "ru-RU")
